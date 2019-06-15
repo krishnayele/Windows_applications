ca9056b310ab5831f902654c3c2639e2c588a772
Krishna Yele <krishnayele@gmail.com>
2019-06-15 23:05:15 +0530

    Add files via upload

---
+++ Services/ShowDataCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace CheckMyDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x => {
+                x.Service<ShowData>(s => {
+                    s.ConstructUsing(showDataObj =>new ShowData());
+                    s.WhenStarted(showDataObj => showDataObj.Start());
+                    s.WhenStopped(showDataObj => showDataObj.Stop());
+
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("ShowDbData");
+                x.SetDisplayName("Show DB Data");
+                x.SetDescription("This service will in 100 sec interval retrieve the info from database and store the number of Items in a File");
+
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetType());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ Services/ShowDataCount/ShowData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace CheckMyDatabase
+{
+    public class ShowData
+    {
+        private readonly Timer _timer;
+        
+
+        public ShowData()
+        {
+            _timer = new Timer(100000) { AutoReset = true };
+            _timer.Elapsed += timer_Elapsed;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Inventory"].ConnectionString;
+            string query = "Select Count(*) from Item;";
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            try
+            {
+                conn.Open();
+               var result =  "\n The number of Items Present is "+cmd.ExecuteScalar();
+                File.AppendAllText(@"E:\practice\C#\services\showData.txt", result.ToString());
+                conn.Close();
+            }catch(Exception ex)
+            {
+                var result = "\n" + ex.Message + "\n";
+                File.AppendAllText(@"E:\practice\C#\services\showData.txt", result.ToString());
+            }
+        }
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
