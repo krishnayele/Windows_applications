1af185e9a21cbc867f86a6a6e0215dca32051777
Krishna Yele <krishnayele@gmail.com>
2019-06-15 17:12:45 +0530

    Add files via upload

---
+++ Services/IcanDoIt/ICanDoIt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ServiceDemo
+{
+    public class ICanDoIt
+    {
+        private readonly Timer _timer;
+        public ICanDoIt()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += timer_Elapsed;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] line = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"E:\practice\C#\services\ICan.txt",line);
+        }
+
+        public void start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Services/IcanDoIt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<ICanDoIt>(s =>
+                {
+                    s.ConstructUsing(iCan => new ICanDoIt());
+                    s.WhenStarted(iCan => iCan.start());
+                    s.WhenStopped(iCan => iCan.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("ICanDoIt");
+                x.SetDisplayName("I Can Do It");
+                x.SetDescription("It is a demo service that will run in background");
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
